d4f24b8b4613a195f56e47bcac21895a961b9a1a
Gautier Nilhcem <nilhcem@gmail.com>
2016-04-03 21:25:39 +0200

    Add Retrofit, Moshi, OkHttp

--- scraper/src/main/kotlin/com/nilhcem/devoxxfr/scraper/App.kt
+++ scraper/src/main/kotlin/com/nilhcem/devoxxfr/scraper/App.kt
@@ -1,5 +1,9 @@
 package com.nilhcem.devoxxfr.scraper
 
+import com.nilhcem.devoxxfr.scraper.api.DevoxxApi
+
 fun main(args: Array<String>) {
-    println("Hi!")
+    val api = DevoxxApi.SERVICE
+    val me = api.getSpeakerDetails("9cc5353ec72a6d4614a652eae245ff5bdd260b58").execute().body()
+    println(me)
 }
---
+++ scraper/src/main/kotlin/com/nilhcem/devoxxfr/scraper/api/DevoxxApi.kt
@@ -0,0 +1,28 @@
+package com.nilhcem.devoxxfr.scraper.api
+
+import com.nilhcem.devoxxfr.scraper.model.devoxx.Speaker
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DevoxxApi {
+
+    companion object {
+        private val ENDPOINT = "https://cfp.devoxx.fr/api/conferences/devoxxFR2016/"
+        private val MOSHI = MoshiConverterFactory.create(Moshi.Builder().build())
+
+        val SERVICE = Retrofit.Builder()
+                .client(OkHttpClient())
+                .baseUrl(ENDPOINT)
+                .addConverterFactory(MOSHI)
+                .build()
+                .create(DevoxxApi::class.java)
+    }
+
+    @GET("speakers/{uuid}")
+    fun getSpeakerDetails(@Path("uuid") uuid: String): Call<Speaker>
+}
