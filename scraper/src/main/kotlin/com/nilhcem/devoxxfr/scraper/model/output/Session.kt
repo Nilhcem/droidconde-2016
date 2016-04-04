c6368fad8a317c8054537144862244f2009f9b45
Gautier Nilhcem <nilhcem@gmail.com>
2016-04-04 21:41:05 +0200

    Create output model

---
+++ scraper/src/main/kotlin/com/nilhcem/devoxxfr/scraper/model/output/Session.kt
@@ -0,0 +1,3 @@
+package com.nilhcem.devoxxfr.scraper.model.output
+
+data class Session(val id: Int, val startAt: String, val duration: Int, val roomId: Int, val speakersId: List<Int>, val title: String, val description: String)
---
+++ scraper/src/main/kotlin/com/nilhcem/devoxxfr/scraper/model/output/Speaker.kt
@@ -0,0 +1,4 @@
+package com.nilhcem.devoxxfr.scraper.model.output
+
+data class Speaker(val id: Int, val name: String, val title: String, val bio: String,
+                   val website: String, val twitter: String, val photo: String)
