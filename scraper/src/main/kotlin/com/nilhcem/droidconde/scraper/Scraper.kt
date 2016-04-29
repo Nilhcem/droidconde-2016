852b807e6c3dee780830209ca426373477789cd1
Gautier Nilhcem <nilhcem@gmail.com>
2016-04-29 13:48:44 +0200

    Improve scraper

--- scraper/src/main/kotlin/com/nilhcem/droidconde/scraper/Scraper.kt
+++ scraper/src/main/kotlin/com/nilhcem/droidconde/scraper/Scraper.kt
@@ -3,6 +3,9 @@ package com.nilhcem.droidconde.scraper
 import com.nilhcem.droidconde.scraper.model.droidcon.Session
 import com.nilhcem.droidconde.scraper.model.droidcon.Speaker
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.safety.Whitelist
+import org.jsoup.select.Elements
 
 class Scraper {
 
@@ -13,20 +16,20 @@ class Scraper {
 
     fun getSpeakers(): List<Speaker> {
         System.out.println("Get speakers");
-        return Jsoup.connect(SPEAKERS_URL).get().select("div.view-content .views-row")
+        return jsoup(SPEAKERS_URL).select("div.view-content .views-row")
                 .map {
                     it.select(".views-field-field-profile-last-name .field-content a").attr("href")
                 }
                 .distinct()
                 .mapIndexed { index, url ->
                     System.out.println("Get speaker: #" + Integer.toString(index) + " - url: " + url);
-                    val speakerDoc = Jsoup.connect(url).get().select("#main-content")
+                    val speakerDoc = jsoup(url).select("#main-content")
 
-                    val name = speakerDoc.select("#main-content-header h1").text()
+                    val name = speakerDoc.select("#main-content-header h1").fmtText()
                     val photo = speakerDoc.select(".image-style-profile-default").attr("src")
-                    val job = speakerDoc.select(".views-field-field-profile-job-title div").text()
-                    val company = speakerDoc.select(".views-field-field-profile-organization div a").text()
-                    val bio = speakerDoc.select(".views-field-field-profile-vita div").text()
+                    val job = speakerDoc.select(".views-field-field-profile-job-title div").fmtText()
+                    val company = speakerDoc.select(".views-field-field-profile-organization div a").fmtText()
+                    val bio = speakerDoc.select(".views-field-field-profile-vita div").fmtText()
 
                     val links = speakerDoc.select(".views-field-field-profile-links a").map { it.attr("href") }
                     val sessions = speakerDoc.select(".view-ncms-speaker-sessions-current a").map { it.attr("href") }
@@ -43,15 +46,39 @@ class Scraper {
                 .mapIndexed { index, url ->
                     val fullUrl = "$BASE_URL$url"
                     System.out.println("Get session: #" + Integer.toString(index) + " - url: " + fullUrl);
-                    val sessionDoc = Jsoup.connect(fullUrl).get().select(".block-content")
+                    val sessionDoc = jsoup(fullUrl).select(".block-content")
 
-                    val title = sessionDoc.select(".node-header h1 a").text()
-                    val description = sessionDoc.select(".field-name-field-session-description .field-items").text()
+                    val title = sessionDoc.select(".node-header h1 a").fmtText()
+                    val description = sessionDoc.select(".field-name-field-session-description .field-items").fmtText()
 
                     val speakersIds = speakers
                             .filter { it.sessions.contains(url) }
                             .map { it.id }
                     Session(index, url, title, description, speakersIds)
                 }
+    }
+
+    private fun jsoup(url: String, nbRetries: Int = 3): Document {
+        (0..nbRetries).forEach {
+            try {
+                return Jsoup.connect(url).get()
+            } catch (e: Exception) {
+                System.err.println("Error: ${e.message}. Retry");
+                if (it == nbRetries - 1) {
+                    throw e
+                }
+            }
+        }
+        throw IllegalStateException("This should not happen")
+    }
+
+    private fun Elements.fmtText(): String {
+        var html = Jsoup.clean(html(), "", Whitelist.basic(), Document.OutputSettings().prettyPrint(false))
+        html = html.replace(Regex("<br[ /]*>", RegexOption.IGNORE_CASE), "\n")
+        html = html.replace("<p>", "").replace("</p>", "\n")
+        html = html.replace(Regex("\\s*\n\\s*"), "\n")
+        html = html.replace(Regex("^\n"), "")
+        html = html.replace(Regex("\n$"), "")
+        return html
     }
 }
