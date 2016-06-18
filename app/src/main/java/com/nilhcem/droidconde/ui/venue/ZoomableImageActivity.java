c68c2f7104d11888a96350df4c41b2141f7044af
Gautier Nilhcem <nilhcem@gmail.com>
2016-06-18 20:50:48 +0200

    Remove unused import

--- app/src/main/java/com/nilhcem/droidconde/ui/venue/ZoomableImageActivity.java
+++ app/src/main/java/com/nilhcem/droidconde/ui/venue/ZoomableImageActivity.java
@@ -1,6 +1,5 @@
 package com.nilhcem.droidconde.ui.venue;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
